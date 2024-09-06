255a4de074323e10db760956d21295a4775ffdaa
Terje Karlsen <terjeofnorway@gmail.com>
2024-09-06 12:47:10 +0200

    Legger til port i vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import sass from 'sass';
 
-const isLocal = process.env.VITE_ENV === 'local';
+const port = parseInt(process.env.VITE_PORT || '3000', 10);
 
 export default defineConfig({
     build: {
         outDir: 'build',
+    },
+    server: {
+        port,
     },
     base: process.env.VITE_PUBLIC_URL,
     plugins: [react()],
